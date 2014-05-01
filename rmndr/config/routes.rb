219f01e3571693000dd46c043957f01a87620787
eRenan <yuangao@eRenans-MacBook-Air.local>
2014-04-30 23:30:30 -0700

    Added Delete functionality to RMNDR

--- rmndr/config/routes.rb
+++ rmndr/config/routes.rb
@@ -1,6 +1,9 @@
 Rmndr::Application.routes.draw do
   devise_for :users
   root 'main#index'
+  resources :links
+  resources :reminders
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
