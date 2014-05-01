07b520f8831a658740fd3dab922b2969b0a54b3a
eRenan <yuangao@eRenans-MacBook-Air.local>
2014-04-30 20:47:58 -0700

    started 2 projects KMS and rmndr

---
+++ kmssys/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kmssys::Application.config.secret_key_base = '2b961032bf66a35814218775e0bebf15b7d027b3b6eabdb0ec5ff58b1aa5239ebae32f4d303c3825dbf50681adc96d44b985143771042df1a5252acd896e2a59'
---
+++ kmssys/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kmssys::Application.config.session_store :cookie_store, key: '_kmssys_session'
